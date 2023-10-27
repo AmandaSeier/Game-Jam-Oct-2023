bfa40e4328f704099f4d9c682f45ddd8b14159fe
J0pper <Jeppe.vadandersen@gmail.com>
2023-10-27 19:19:05 +0200

    Indholdet fra de to første tekstfiler

--- node_2d.gd
+++ node_2d.gd
@@ -3,9 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Dialogic.start("res://timeline.dtl")
-
-
+	Dialogic.start("res://OG Timeline.dtl")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
